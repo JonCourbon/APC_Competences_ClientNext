ac52b54fe6808628234e0a8b71539e6ca6553849
JonCourbon <joncourbon@gmail.com>
2021-04-27 23:56:58 +0200

    Correction Footer et modif apparance SAE

--- pages/sae/[id].js
+++ pages/sae/[id].js
@@ -14,7 +14,16 @@ const SAE = ({sae}) => {
   return (
     <Layout>
     <Section>
-    <h2>{sae.intitule}</h2>
+    <div>
+      <h2>{sae.intitule}</h2>
+      <p>{sae.descriptif}</p>
+      <p>Il y a {sae.apprentissage_critiques.length} apprentissage(s) en lien avec cet atelier:</p>
+      <ul>
+      {sae.apprentissage_critiques.map((apprentissage) => (
+        <li>{apprentissage.competence}  > {apprentissage.niveau_competence} > {apprentissage.intitule}</li>
+      ))}
+      </ul>
+    </div>
     </Section>
     </Layout>
   );
@@ -22,6 +31,17 @@ const SAE = ({sae}) => {
 
 export const getStaticProps = async ({ params }) => {
   const sae = await fetchFromCMS(`saes/${params.id}`);
+  for (let i in sae.apprentissage_critiques) {
+    let apprentissage=await fetchFromCMS(`apprentissage-critiques/${sae.apprentissage_critiques[i].id}`);
+    let niveau_competences=await fetchFromCMS(`niveau-competences/${apprentissage.niveau_competence.id}`);
+    
+    sae.apprentissage_critiques[i].niveau_competence=niveau_competences.intitule;
+    sae.apprentissage_critiques[i].competence=String(niveau_competences.competence.intitule).slice(0,10);
+  }
+  
+  sae.apprentissage_critiques.sort((a, b) => {
+      return a.competence - b.competence;
+  });
 
   return {
     props: {
