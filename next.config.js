5d75d24b51108916cb4d26a83cb2cd4b36548f10
JonCourbon <joncourbon@gmail.com>
2021-07-06 11:46:29 +0200

    Ajout fichier de config avec l'id du diplome et l'url de l'API

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  serverRuntimeConfig: {
+    apiurl: "http://localhost:1337",
+    diplome: "2"
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+  },
+}
